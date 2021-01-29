973dca131bf68e984d55faa278c8990a5a35ba14
rexaevum <rolans2k@gmail.com>
2021-01-28 21:38:04 -0500

    needed files for deployment to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // init app 
 const app = express();
@@ -11,14 +12,23 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false})); // to parse the req.body
 
-//test 
-app.get('/', (req, res) => res.send(`API running`))
 
 // define route for users
 app.use('/api/users', require(`./routes/api/users`));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profiles'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production (has to be after routs)
+if (process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static('client/build'));
+
+    // serve
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // get the port from host or run on 5000
 const PORT = process.env.PORT || 5000;
